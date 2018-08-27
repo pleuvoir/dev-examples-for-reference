684bfbc35cace1625ad35101bcabb67d2aa7abcc
pleuvoir <mailto:pleuvior@foxmail.com>
2018-08-27 20:01:43 +0800

    :blush: update const

--- spring-scheduler-example/src/main/java/io/github/pleuvoir/kit/QuartzConst.java
+++ spring-scheduler-example/src/main/java/io/github/pleuvoir/kit/QuartzConst.java
@@ -4,13 +4,13 @@ public class QuartzConst {
 
 	public static class JobKeyGroup {
 
-		public static final String NORMAL_JOB = "nornalJob";
+		public static final String NORMAL_JOB = "normalJob";
 
 	}
 
 	public static class TriggerKeyGroup {
 
-		public static final String NORMAL_JOB = "nornalJob";
+		public static final String NORMAL_JOB = "normalJob";
 
 	}
 }
