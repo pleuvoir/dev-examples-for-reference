fa5edbd27e38942750c84b3b547d89fbf958086e
pleuvoir <pleuvior@foxmail.com>
2018-12-20 14:34:39 +0800

    stomp 推送

---
+++ stomp-sample/src/main/java/io/github/pleuvoir/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package io.github.pleuvoir.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/*
+ * 开启使用Stomp协议来传输基于消息broker的消息 这时控制器支持使用@MessageMapping，就像使用@RequestMapping一样
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		/*
+		 * 注册STOMP协议的节点(endpoint),并映射指定的url,
+		 * 添加一个访问端点“/chatroom”,客户端打开双通道时需要的url, 允许所有的域名跨域访问，指定使用SockJS协议。
+		 */
+		registry.addEndpoint("/chatroom").setAllowedOrigins("*").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		/*
+		 * 配置一个消息代理 topic 负责群聊  queue 单聊
+		 */
+		registry.enableSimpleBroker("/topic", "/queue");
+
+		// 一对一的用户，请求发到/queue  即 convertAndSendToUser 方法会在回传给前端消息地址前追加此前缀
+		registry.setUserDestinationPrefix("/queue");
+	}
+
+}
---
+++ stomp-sample/src/main/java/io/github/pleuvoir/web/StompController.java
@@ -0,0 +1,45 @@
+package io.github.pleuvoir.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import io.github.pleuvoir.domain.ChatRoomRequest;
+import io.github.pleuvoir.domain.ChatRoomResponse;
+
+@Controller
+public class StompController {
+
+	@Autowired
+	private SimpMessagingTemplate template;/* Spring实现的一个发送模板类 */
+
+	/* 消息群发，接受发送至自massRequest的请求 */
+	@MessageMapping("/massRequest")
+	@SendTo("/topic/getResponse")
+	// SendTo 发送至 Broker 下的指定订阅路径topic , 所以这个开头一定要是注册过的 否则无法代理使用 registry.enableSimpleBroker("/topic", "/queue");
+	// Broker再根据getResponse发送消息到订阅了/mass/getResponse的用户处
+	public ChatRoomResponse mass(ChatRoomRequest chatRoomRequest) {
+		System.out.println("name =" + chatRoomRequest.getName() + " chatValue =" + chatRoomRequest.getChatValue());
+		ChatRoomResponse response = new ChatRoomResponse();
+		response.setName(chatRoomRequest.getName());
+		response.setChatValue(chatRoomRequest.getChatValue());
+		// this.template.convertAndSend();
+		return response;
+	}
+
+	/* 单独聊天，接受发送至自aloneRequest的请求 */
+	@MessageMapping("/aloneRequest")
+	// @SendToUser
+	public ChatRoomResponse alone(ChatRoomRequest chatRoomRequest) {
+		System.out.println("SendToUser = " + chatRoomRequest.getUserId() + " FromName = " + chatRoomRequest.getName()
+				+ " ChatValue = " + chatRoomRequest.getChatValue());
+		ChatRoomResponse response = new ChatRoomResponse();
+		response.setName(chatRoomRequest.getName());
+		response.setChatValue(chatRoomRequest.getChatValue());
+		// 发送地址会变为 {userid}/alone ==> /queue/{userid}/alone  增加的前缀是  registry.setUserDestinationPrefix("/queue") 设置的
+		this.template.convertAndSendToUser(chatRoomRequest.getUserId(), "/alone", response);
+		return response;
+	}
+}
